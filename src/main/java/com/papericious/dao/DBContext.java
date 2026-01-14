85d202a84db0b5f509b8769161858a524849b047
kimky <kyvkhe182094@fpt.edu.vn>
2026-01-14 22:58:33 +0700

    update db context for backend

---
+++ src/main/java/com/papericious/dao/DBContext.java
@@ -0,0 +1,30 @@
+package com.papericious.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+    private final String url = "jdbc:mysql://localhost:3306/kisskit_db?useSSL=false&serverTimezone=UTC";
+    private final String user = "root";
+    private final String password = "1234";
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(e);
+        }
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        try (Connection connection = db.getConnection()) {
+            System.out.println("Connected to database: " + connection.getCatalog());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
